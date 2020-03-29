49e1f552f247b5af5958cbff9c3cdd9c77274dab
harish <harish.ganesh369@gmail.com>
2020-03-29 21:59:43 +0530

    changed fun calls

--- api.py
+++ api.py
@@ -15,25 +15,26 @@ if file_bytes is not None:
     file_str = file_bytes.read()
     data = json.loads(file_str)
 
-    location_data,demand,vehicle_capacity,depot,df,lat_long,vehicle_cost = get_solution(data)
-
+    demand,vehicle_capacity,depot,lat_long,vehicle_cost,unknown_address = get_solution(data)
+    if len(unknown_address) > 0:
+         st.info('Addresses shown below are not found!')
+         st.write(unknown_address)
+    else:
      #getting distance matrix
-    distance_matrix = get_distance_matrix(location_data, len(lat_long))
+        distance_matrix = get_distance_matrix()
 
-    data = {}
-    data['distance_matrix'] = distance_matrix
-    data['demands'] = demand
-    data['vehicle_capacities'] = vehicle_capacity
-    data['num_vehicles'] = len(vehicle_capacity)
-    data['depot'] = depot
-    data['vehicle_costs'] = vehicle_cost
+        data = {}
+        data['distance_matrix'] = distance_matrix
+        data['demands'] = demand
+        data['vehicle_capacities'] = vehicle_capacity
+        data['num_vehicles'] = len(vehicle_capacity)
+        data['depot'] = depot
+        data['vehicle_costs'] = vehicle_cost
 
-   
-
-    #define solver with penality as 5000 and 120seconds as maximum running time
-    solver = VrpSolver(5000,120)
-    output = solver.solve(data)
-    #st.balloons()
-    st.title('Result:')
-    st.write(output)
-    get_folium_map(df,output,lat_long,st)
+        #define solver with penality as 5000 and 120seconds as maximum running time
+        solver = VrpSolver(5000,120)
+        output = solver.solve(data)
+        #st.balloons()
+        st.title('Result:')
+        st.write(output)
+        get_folium_map(df,output,lat_long,st)
