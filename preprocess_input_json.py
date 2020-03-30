dac04ad9bd728dca031045855e883dc73a732860
Midhilesh29 <midhileshgithub@gmail.com>
2020-03-30 19:16:49 +0530

    Bug fixed

--- preprocess_input_json.py
+++ preprocess_input_json.py
@@ -3,16 +3,22 @@ from geocoding import get_lat_long
 
 
 def get_solution(Input):
+    print("Input:")
+    print(Input)
+    print("\n")
     distribution_address = []
     delivery_address = []
     demand = []
     vehicle_capacity = []
     depot = []
+    vehicle_speed = []
     vehicle_cost = []
+
     for index, values in enumerate(Input['distribution_pts']):
         distribution_address.append(values['address'])
         vehicle_capacity.extend(values['vehicle_capacity'])
         vehicle_cost.extend(values['vehicle_cost'])
+        vehicle_speed.extend(values['vehicle_speed'])
         for i in range(len(values['vehicle_capacity'])):
             depot.append(index)
         demand.append(0)
@@ -23,12 +29,15 @@ def get_solution(Input):
     distribution_address_data = get_lat_long(distribution_address)
     delivery_address_data = get_lat_long(delivery_address)
 
-    unknown_address = list
-    unknown_address.append(distribution_address_data['unknown_address'])
-    unknown_address.append(delivery_address_data['unknown_address']) 
+    unknown_address = list()
+    print("\n")
+    print("distribution address:", distribution_address_data)
+    print("delivery address:", delivery_address_data)
+    unknown_address.extend(distribution_address_data['unknown_address'])
+    unknown_address.extend(delivery_address_data['unknown_address']) 
    
     lat_long = list()
     lat_long.extend(distribution_address_data['lat_long'])
     lat_long.extend(delivery_address_data['lat_long'])
 	
-    return(demand,vehicle_capacity,depot,lat_long,vehicle_cost,unknown_address)
+    return(demand,vehicle_capacity,vehicle_speed,depot,lat_long,vehicle_cost,unknown_address)
