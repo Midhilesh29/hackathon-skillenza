9031c12498e38cf6cb1d6084c678428f90e0774c
harish <harish.ganesh369@gmail.com>
2020-03-31 17:04:30 +0530

    added details in folium map

--- preprocess_input_json.py
+++ preprocess_input_json.py
@@ -31,10 +31,10 @@ def get_solution(Input):
         demand.append(values['demand'])
     # geocoding
     distribution_address_data = get_lat_long(distribution_address)
-    delivery_address_data = get_lat_long(delivery_address)
+    #delivery_address_data = get_lat_long(delivery_address)
 
-    '''
-    cache data
+    #'''
+    #cache data
 
     
     distribution_address_data = {'lat_long': [[77.01372,11.02543], [77.87545,29.85297]], 
@@ -43,7 +43,7 @@ def get_solution(Input):
     delivery_address_data = {'lat_long': [[72.91116,19.13393], [78.56615,17.57336], [77.18362,28.54057]], 
     'known_address': ['iit bombay search results main gate rd iit area powai mumbai maharashtra 400076', 'bits pilani shamirpet keesara road jawahar nagar shameerpet hyderabad telangana 500078', 'iit delhi iit campus hauz khas new delhi delhi 110016'], 
     'unknown_address': []}
-    '''
+    
 
     
     unknown_address = list()
@@ -56,5 +56,7 @@ def get_solution(Input):
     lat_long = list()
     lat_long.extend(distribution_address_data['lat_long'])
     lat_long.extend(delivery_address_data['lat_long'])
-	
-    return(demand,vehicle_capacity,vehicle_speed, vehicle_max_time,vehicle_max_path_length,depot, lat_long,vehicle_cost,unknown_address)
+    address = []
+    address.extend(distribution_address)
+    address.extend(delivery_address)
+    return(demand,vehicle_capacity,vehicle_speed, vehicle_max_time,vehicle_max_path_length,depot, lat_long,vehicle_cost,unknown_address,address)
